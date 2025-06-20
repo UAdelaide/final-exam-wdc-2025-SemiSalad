7d6fb863841968081a5928babd5252a11d4f5405
OscaR <semisalad@gmail.com>
2025-06-20 05:20:19 +0000

    [automatic commit] 2025-06-20 05:20:19 UTC 49.178.205.114

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -63,7 +63,8 @@ router.post('/logout', (req, res) => {
     if (err) {
       return res.status(500).json({ error: 'Logout failed' });
     }
-    res.clearCookie('connect.')
+    res.clearCookie('connect.sid');
+    res.json({ message: 'Logout successful' });
   })
 })
 
