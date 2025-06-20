33cb104af5cd36666a337b9de9e694ffed4c8119
OscaR <semisalad@gmail.com>
2025-06-20 01:49:57 +0000

    [automatic commit] 2025-06-20 01:49:57 UTC 49.178.205.114

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -65,4 +65,4 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('notadog', 'notadog@definitelynotadog.com', 'woof', 'walker');
 
 INSERT INTO Dogs (owner_id, name, size) VALUES
-((SELECT ))
+((SELECT user_id FROM Users WHERE username = 'alice123'), )
