2c724674fc0b86a1645e9fa39ee585a4da195040
OscaR <semisalad@gmail.com>
2025-06-20 04:42:35 +0000

    [automatic commit] 2025-06-20 04:42:35 UTC 49.178.205.114

--- part2/app.js
+++ part2/app.js
@@ -8,6 +8,9 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(session{
+    secret:
+})
 
 // Routes
 const walkRoutes = require('./routes/walkRoutes');
