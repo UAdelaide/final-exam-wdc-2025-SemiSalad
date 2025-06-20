f02a9dfe5b6adbdc15e7def50a656bee7c62326f
OscaR <semisalad@gmail.com>
2025-06-20 02:33:29 +0000

    [automatic commit] 2025-06-20 02:33:29 UTC 49.178.205.114

--- part1/app.js
+++ part1/app.js
@@ -79,6 +79,7 @@ app.get('/api/walkers/summary', async (req, res) => {
       SELECT u.username AS walker_username,
       COUNT(r.rating_id) AS total_ratings,
       AVG(r.rating) AS average_rating,
+      COUNT()
       `)
   }
 })
