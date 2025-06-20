dd53b35aa82bea45f1676dfbedde7b2d01a5d463
OscaR <semisalad@gmail.com>
2025-06-20 02:24:56 +0000

    [automatic commit] 2025-06-20 02:24:56 UTC 49.178.205.114

--- part1/app.js
+++ part1/app.js
@@ -57,7 +57,9 @@ app.get('/api/dogs', async (req, res) => {
 
 app.get('/api/walkrequests/open', async (req, res) => {
   try {
-    const [rows] = await db.execute(``)
+    const [rows] = await db.execute(`
+      
+      `)
   }
 })
 
