af61ca07962647b3bcdb5353afd21f637e6f6074
OscaR <semisalad@gmail.com>
2025-06-20 01:52:21 +0000

    [automatic commit] 2025-06-20 01:52:21 UTC 49.178.205.114

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -71,4 +71,4 @@ INSERT INTO Dogs (owner_id, name, size) VALUES
 ((SELECT user_id FROM Users WHERE username = 'johnwick'), 'Boy', 'small'),
 ((SELECT user_id FROM Users WHERE username = 'notadog'), 'Dog', 'large'),
 
-INSERT INTO WalkRequests ()
+INSERT INTO WalkRequests (dog_id, requested_time, duration_minutes, )
