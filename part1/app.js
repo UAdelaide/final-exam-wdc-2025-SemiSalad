7c8f60fe3e1e3284429fb377358eade8be70d4c9
OscaR <semisalad@gmail.com>
2025-06-20 02:16:40 +0000

    [automatic commit] 2025-06-20 02:16:40 UTC 49.178.205.114

--- part1/app.js
+++ part1/app.js
@@ -44,7 +44,10 @@ let db;
 app.get('/api/dogs', async (req, res) => {
   try {
     const [rows] = await db.execute(`
-      SELECT d.name AS dog_name, d.size, u.username AS owner_username`)
+      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      FROM Dogs d JOIN Users u ON d.owner_id = u.user_id
+      `);
+      res.json(rows);
   }
 })
 
