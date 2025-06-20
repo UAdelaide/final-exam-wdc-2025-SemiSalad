cd404a389ba4a591bdb045ed525ba33246610606
OscaR <semisalad@gmail.com>
2025-06-20 06:12:22 +0000

    [automatic commit] 2025-06-20 06:12:22 UTC 49.178.205.114

--- part2/routes/userRoutes.js
+++ part2/routes/userRoutes.js
@@ -89,7 +89,7 @@ router.get('/ownedDogs', async (req, res) => {
 app.get('/api/dogs', async (req, res) => {
   try {
     const [dogs] = await db.execute(`
-      SELECT d.name AS dog_name, d.size, u.username AS owner_username
+      SELECT d.dog_id, d.name AS dog_name, d.size, u.username AS owner_username
       FROM Dogs d JOIN Users u ON d.owner_id = u.user_id
       `);
       res.json(dogs);
