42718122e445a96041cd3a003b65ac7182fd9247
OscaR <semisalad@gmail.com>
2025-06-20 01:47:33 +0000

    [automatic commit] 2025-06-20 01:47:33 UTC 49.178.205.114

--- part1/dogwalks.sql
+++ part1/dogwalks.sql
@@ -61,4 +61,5 @@ INSERT INTO Users (username, email, password_hash, role) VALUES
 ('alice123', 'alice@example.com', 'hashed123', 'owner'),
 ('bobwalker', 'bob@example.com', 'hashed456', 'walker'),
 ('carol123', 'carol@example.com', 'hashed789', 'owner'),
-('johnwick', j)
+('johnwick', 'john@wick.com', 'dogiscool', 'owner'),
+('doglover')
